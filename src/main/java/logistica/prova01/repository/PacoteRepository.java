71a0fcd076ab8225c3a9c5b634002684d140c24c
ViniRaphael <vinicius.raphael1311@gmail.com>
2024-08-01 20:11:15 -0300

    Criação da Camada Repository

---
+++ src/main/java/logistica/prova01/repository/PacoteRepository.java
@@ -0,0 +1,8 @@
+package logistica.prova01.repository;
+
+import logistica.prova01.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacoteRepository extends JpaRepository<Pacote, String> {
+}
+
